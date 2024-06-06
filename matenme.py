ede241e2e95923e397dc6913d0b4cc162c6bac56
anto <abravolozano@safareyes.es>
2024-06-06 12:14:46 +0200

    he terminado el programa de LuisPuto

--- matenme.py
+++ matenme.py
@@ -4,7 +4,13 @@ import json
 with open('mensajes.json', 'r', encoding='utf8') as _f:
     _Datos = json.load(_f)
 
-# while True:
-    print(_Datos['pregunta'])
-    _r = input()
+while True:
+    print(_Datos['pregunta'], _Datos['acciones'])
+    _r = int(input())
+
+    if _r == 1:
+        break
+
+    print(_Datos['no'])
     
+print(_Datos['si'])
