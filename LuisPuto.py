76fdd245fe626a8189098faecdd56b2407a8df70
anto <abravolozano@safareyes.es>
2024-06-06 12:53:03 +0200

    cambiado el sistema para que pasar el valor del nombre o Luis

--- LuisPuto.py
+++ LuisPuto.py
@@ -1,8 +1,12 @@
 import json
+import sys
 
+_nombre = "".join(sys.argv[1:]) if len(sys.argv) > 1 else 'Luis'
 
 with open('mensajes.json', 'r', encoding='utf8') as _f:
     _Datos = json.load(_f)
+    for _k, _i in _Datos.items():
+        _Datos[_k] = _i.replace('###', _nombre)
 
 while True:
     print(_Datos['pregunta'], _Datos['acciones'])
